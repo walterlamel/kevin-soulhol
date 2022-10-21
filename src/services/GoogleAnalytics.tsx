1d6e21fdf54caf5d2fe196135edde664599fe60a
walter <kevin.soulhol@gmail.com>
2022-10-21 07:42:32 +0200

    change tracking id google

--- src/services/GoogleAnalytics.tsx
+++ src/services/GoogleAnalytics.tsx
@@ -1,12 +1,13 @@
 import ReactGA from 'react-ga';
 
-export const TRACKING_ID = "G-PM8XC1VWGM"; // TRACKING_ID
+export const TRACKING_ID = "UA-207358357-1"; // TRACKING_ID
+//export const TRACKING_ID = "G-PM8XC1VWGM"; // TRACKING_ID google analytics 4
 
 
 /**
  * Hook qui permet d'envoyer l'action à google Analytics
  * @param category The type of interaction (e.g. 'play')
- * @returns 
+ * @returns void
  */
 const useAnalyticsEventTracker = (category : string) => {
     const eventTracker = (action: string, label ?: string) => {
